4b556c9f0bf1c4e182538687e9583b1ddb34e18f
MalikAQayum <l_malik_l@hotmail.com>
2017-01-28 01:24:38 +0100

    update 1.7.4
    update 1.7.4

--- Steam-Tracker.meta.js
+++ Steam-Tracker.meta.js
@@ -39,7 +39,7 @@
 // @exclude      *://steamcommunity.com/app/*/reviews*
 // @downloadURL https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.user.js
 // @updateURL   https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.meta.js
-// @version      1.7.3
+// @version      1.7.4
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
