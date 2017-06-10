44d23f9493cec413ce70a853791b793287ac23ec
MalikAQayum <l_malik_l@hotmail.com>
2017-06-11 01:43:43 +0200

    Create Steam-Tracker.user.js

--- Steam-Tracker.user.js
+++ Steam-Tracker.user.js
@@ -19,7 +19,7 @@
 // @require     https://raw.githubusercontent.com/MalikAQayum/SteamTracker/master/GMDelete.js
 // @downloadURL https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.user.js
 // @updateURL   https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.user.js
-// @version      5.0.0.9
+// @version      5.0.0.10
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
