fd4906bc405204852a3ad88e33e96124ac5364d8
MalikAQayum <l_malik_l@hotmail.com>
2018-01-01 19:27:19 +0100

    // @version      5.0.1.6
    2025 charts

--- Steam-Tracker.user.js
+++ Steam-Tracker.user.js
@@ -24,7 +24,7 @@
 // @require     https://raw.githubusercontent.com/MalikAQayum/SteamTracker/master/GMDelete.js
 // @downloadURL https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.user.js
 // @updateURL   https://github.com/MalikAQayum/SteamTracker/raw/master/Steam-Tracker.user.js
-// @version      5.0.1.5
+// @version      5.0.1.6
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
